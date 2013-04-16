0b92caf155793dcf3b62a1711c659022fc6cf32c
Junhyun Shin <hl1sqi@gmail.com>
2013-04-16 18:01:15 +0900

    create reservation controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,7 @@
 Petbnb::Application.routes.draw do
 
-  get "reservations/index"
 
-  get "reservations/show"
-
-  get "reservations/new"
-
-  get "reservations/create"
-
-  get "reservations/edit"
-
-  get "reservations/update"
-
-  get "reservations/destroy"
-
-  resources :dummies, :homes, :pet_houses
+  resources :dummies, :homes, :pet_houses, :reservations
   
   root to: 'homes#index'
 
